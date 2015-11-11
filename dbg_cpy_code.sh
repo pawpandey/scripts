36ee5aef991ec9ed65eeecb8d176f18c01de7af4
Blake Triana <blake.triana@gmail.com>
2015-11-10 18:45:17 -0800

    Changes to dbg_cpy_code.sh

--- dbg_cpy_code.sh
+++ dbg_cpy_code.sh
@@ -2,5 +2,13 @@ SOURCE_DIR="/Users/Blake/shadow_0/Assets/Code"
 #DEST_DIR="/Users/Blake/Desktop/foo"
 DEST_DIR="/Users/Blake/shadow_1/Assets/Code"
 
-rm -rf $DEST_DIR
-cp -r $SOURCE_DIR $DEST_DIR
+
+
+if [ $1 -eq 1 ]; then
+    echo "arg was 1"
+elif [ $1 -eq 0 ]; then
+    echo "arg was 2"
+fi
+
+#rm -rf $DEST_DIR
+#cp -r $SOURCE_DIR $DEST_DIR
